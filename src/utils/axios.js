81ca43a58ae6f75d1724182e4251e4c8dd7fb021
NAYEEM65 <mathnayeem054@gmail.com>
2023-01-09 22:43:40 +0600

    server changed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://own-data-server.herokuapp.com/',
+    // baseURL: 'https://own-data-server.herokuapp.com/',
+    baseURL: 'https://video-data-server-lws.vercel.app/'
 });
 
 export default axiosInstance;
